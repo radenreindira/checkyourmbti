9b423ba6d1b91f4a468615a829f935c94db9b237
radenreindira <alkafiath150@gmail.com>
2026-06-25 12:04:31 +0700

    Delete config.js

--- config.js
+++
@@ -1,4 +0,0 @@
-window.MBTI_CONFIG = {
-  SUPABASE_URL: "https://yabvtjgpbelywqfpxmel.supabase.co,
-  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlhYnZ0amdwYmVseXdxZnB4bWVsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODIyMTgyNDQsImV4cCI6MjA5Nzc5NDI0NH0.qlDRJk0JqEDVYi2-lRxv9Qpc6SoyuSBOA_hE_fteSps"
-};
